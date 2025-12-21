5bfcc323fe060bed0ecb8566c462d8178f6aecf3
Prakhar Sharma <prakharsharma_mc24b06_001@dtu.ac.in>
2025-12-22 02:10:14 +0530

    Add files via upload

---
+++ Array_LeetCode/Single-Number/single-number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int x=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            x=x^nums[i];
+        }
+        return x;
+    }
+};
