1e62fabb78a02dc74d184643b9e36fb79707a7e3
Prakhar Sharma <prakharsharma_mc24b06_001@dtu.ac.in>
2025-12-22 23:48:55 +0530

    Add files via upload

---
+++ Array_LeetCode/2-SUM/2_sum.cpp
@@ -0,0 +1,35 @@
+//method 1
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> twoSum(vector<int>& nums, int target) {
+        int length = nums.size();
+
+        int arr[100];
+        for(int i=0;i<length;i++){
+            for(int j=i + 1;j<length;j++){
+                if (nums[i]+nums[j]==target){
+                    return {i, j};
+                }
+               
+            }
+        }
+        return {};
+    }
+
+//method 2
+vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp; 
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            
+            if (mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
+            }
+            
+            mp[nums[i]] = i;
+        }
+        
+        return {}; 
+}
