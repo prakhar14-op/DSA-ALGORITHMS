a4e7e5ca6a9e36ec0b823f69750f95731379d51e
Prakhar Sharma <prakharsharma_mc24b06_001@dtu.ac.in>
2026-01-10 16:14:30 +0530

    Add files via upload

---
+++ Binary Search_leetcode/search-insert-position/search-insert-position.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n=nums.size();
+        int l=0;
+        int h=n-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                l=mid+1;
+            }
+            else{
+                h=mid-1;
+            }
+        }
+        return h+1;
+        
+        
+    }
+};
